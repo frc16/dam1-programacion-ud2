398a54c1e5c382360bcd43e9aaec2f99e2acb1c6
Samu7526 <samueljosue312005@gmail.com>
2025-10-01 14:16:28 +0200

    Update DatosUsuario.java

--- src/DatosUsuario.java
+++ src/DatosUsuario.java
@@ -12,6 +12,8 @@ public class DatosUsuario {
         sc.nextLine();
         String correo = sc.nextLine();
         sc.nextLine();
+
+
         boolean emailValido = ProcesadorTexto.esEmailValido(correo);
         if (emailValido) {
 
@@ -24,6 +26,22 @@ public class DatosUsuario {
 
         }
 
+        // No se si esto era lo que pedia con la edad
+        if (edad >= 120) {
+
+            System.out.println("Error");
+
+        } else if (edad <= 120) {
+
+            System.out.println("El correo esta correcto");
+
+        }
+
+
+        String name = "samuel";
+        System.out.println("El nombre es: " + name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase());
+
+        
         sc.close();
     }
 }
