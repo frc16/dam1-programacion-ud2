f005775d079e7af6220a099c5d6b6088a9acabf5
frc16 <153406170+frc16@users.noreply.github.com>
2025-10-02 09:42:12 +0200

    formateado

--- src/DatosUsusario.java
+++ src/DatosUsusario.java
@@ -6,26 +6,28 @@ public class DatosUsusario {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-      System.out.print("nombre:");
-      String nombre = sc.nextLine();
+        System.out.print("nombre:");
+        String nombre = sc.nextLine();
 
-      System.out.print("edad");
-      int edad = sc.nextInt();
-      sc.nextLine();
+        System.out.print("edad");
+        int edad = sc.nextInt();
+        sc.nextLine();
 
-      System.out.print("correo:");
-      String correo = sc.nextLine();
+        System.out.print("correo:");
+        String correo = sc.nextLine();
 
-      boolean correoValido = ProcesadorTexto.esEmailValido(correo);
-      boolean edadValida = edad < 0 || edad > 120;
+        boolean correoValido = ProcesadorTexto.esEmailValido(correo);
+        boolean edadValida = edad < 0 || edad > 120;
 
-     if(correoValido){ Usuario usuario = new Usuario(nombre, edad, correo);
-     } else { System.out.println("error, esuario no es valido");
+        if (correoValido) {
+            Usuario usuario = new Usuario(nombre, edad, correo);
+        } else {
+            System.out.println("error, esuario no es valido");
 
-     String name = "frank";
-        
+            String name = "frank";
+
+        }
+        sc.close();
     }
-    sc.close();
-    }
-     
+
 }
